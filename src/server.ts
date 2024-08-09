d9a4a50bbd259457fcfe4206eb4f898fea35de99
Vittor Rafael <rafaelsales202205@gmail.com>
2024-08-09 10:26:52 -0300

    ajustando erro no render

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { env } from "./env";
 
 app
   .listen({
+    host: "RENDER" in process.env ? "0.0.0.0" : "localhost",
     port: env.PORT,
   })
   .then(() => console.log("HTTP Server Running"));
